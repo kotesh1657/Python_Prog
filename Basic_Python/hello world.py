a209ad445710f4e8ec14d29b3d8430c45f80517c
kotesh <dteswar@gmail.com>
2022-02-03 22:35:29 +0530

    interactive program

--- Basic_Python/hello world.py
+++ Basic_Python/hello world.py
@@ -1,4 +1,15 @@
 # hello world prog
+import time
 
-user_Input = input("Enter the famous Programming sentence :")
-print(user_Input)
+
+print("hello world")
+
+# using time module
+
+print("ding don bell is ringing")
+usr_Input = input("Who is there : ")
+time.sleep(2)
+deli_Input = input("Tell the house owner why u rung the bell: ")
+time.sleep(1)
+print("Thanks for the service :)")
+
